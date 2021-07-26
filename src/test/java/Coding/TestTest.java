381b8c9952899531571ae8cf3f7abda7d525e9be
lishuai <lishuai0214@gmail.com>
2021-07-26 19:52:30 +0800

    update

--- src/test/java/Coding/TestTest.java
+++ src/test/java/Coding/TestTest.java
@@ -219,4 +219,34 @@ public class TestTest {
         int result11 = climbStairs.climbStairs(30);
         System.out.println("result11: " + result11);
     }
+
+    /*
+    你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+    不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+    说明:
+    为什么返回数值是整数，但输出的答案是数组呢?
+    请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     */
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        int fast = 1;
+        int slow = 1;
+        while (fast < length){
+            if (nums[fast] != nums[fast-1]){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    @Test
+    public void removeDuplicates() {
+        int[] nums = {0,0,1,1,1,2,2,3,3,40};
+        int result000 = removeDuplicates(nums);
+        System.out.println("result000: " + result000);
+        System.out.println(Arrays.toString(nums));
+    }
 }
